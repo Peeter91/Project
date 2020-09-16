156f18a9258a51d25a24311cd74f9640db4c0d59
Peeter91 <peeterraj91@gmail.com>
2020-09-16 09:11:45 +0530

    Fist Project added

---
+++ src/org/sample/Hello.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class Hello {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10)
+		{
+			if(i==7)
+			{
+				continue;
+				
+			}
+			System.out.println(i);
+			i++;
+			
+		}
+	}
+}
---
+++ src/org/sample/Sample.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		int empAge=10, empId=20;
+		if (empAge > 10||empId>20) 
+			System.out.println("Valid Employee");
+			else
+				System.out.println("Invalid employee");
+		
+	}
+
+}
